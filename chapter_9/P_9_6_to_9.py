db79bd08d95f0c10393df795c565a88650956a67
raintle <2162951845@qq.com>
2026-01-18 15:09:19 +0800

    阶段性完成

--- chapter_9/P_9_6_to_9.py
+++ chapter_9/P_9_6_to_9.py
@@ -31,13 +31,49 @@ class IceCreamStand(Restaurant):
         for flavour in self.flavour:
             print(flavour)
         self.all_print()
-old_people_chicken = Restaurant("Chicken", "north")
-#创建实例
-old_people_chicken.all_print()
-#使用方法
-print('\n')
 
-a_ice_cream = IceCreamStand("ice cream", "ice")
-a_ice_cream.append_flavour('fruit')
-a_ice_cream.append_flavour('strew')
-a_ice_cream.print_flavour()
+class User:
+    """user的类"""
+    def __init__(self, first_name, last_name, age, email):
+        """初始化user信息"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+
+    def greet_user(self):
+        """向user打招呼"""
+        print(f"hello! {self.first_name} {self.last_name}.")
+
+    def describe_user(self):
+        """描述user"""
+        print(f"\n{self.first_name} {self.last_name} is {self.age} years old.")
+        print('ur email is', self.email)
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, age, email):
+        super().__init__(first_name, last_name, age, email)
+        self.privileges = ['cam ban user', 'can delete post']
+
+    def show_privileges(self):
+        print(f'about u privileges:{', '.join(self.privileges)}')
+
+
+
+if __name__ == '__main__':
+    old_people_chicken = Restaurant("Chicken", "north")
+    # 创建实例
+    old_people_chicken.all_print()
+    # 使用方法
+    print('\n')
+
+    a_ice_cream = IceCreamStand("ice cream", "ice")
+    a_ice_cream.append_flavour('fruit')
+    a_ice_cream.append_flavour('strew')
+    a_ice_cream.print_flavour()
+    print('\n')
+
+    admin1 = Admin('adw', 'dwa', 12, '<EMAIL>')
+    admin1.greet_user()
+    admin1.show_privileges()
