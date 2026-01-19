596c1897da6202ebb4aa943c24d950009fbc3e75
raintle <2162951845@qq.com>
2026-01-19 09:41:20 +0800

    该练习完成

--- chapter_10/P_10_1_2.py
+++ chapter_10/P_10_1_2.py
@@ -1,7 +1,16 @@
 with open('python知识点.txt', encoding='UTF-8') as f:
+    '''打开该文件，并将该文件称作f'''
     content = f.read()
+    #读取文件中内容
     print(content, '\n')
 
 with open('python知识点.txt', encoding='UTF-8') as f:
     for line in f:
+        #将文件内容每一行视作列表元素
         print(line)
+
+with open('python知识点.txt', encoding='UTF-8') as f:
+    content = f.read()
+    content = content.replace('python', 'c')
+    #文本替换
+    print(content)
