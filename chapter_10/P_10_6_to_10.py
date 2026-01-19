ae13c32cd735fdfe0912ecd2daa21df7efb18902
raintle <2162951845@qq.com>
2026-01-19 14:58:34 +0800

    该练习完成

--- chapter_10/P_10_6_to_10.py
+++ chapter_10/P_10_6_to_10.py
@@ -4,29 +4,35 @@ def try_int():
     while True:
         try:
             num = int(input('number: '))
+            #试图转换成int类型，出错则重新输入
         except ValueError:
             print("Please enter a number.")
         else:
             break
     return num
 
-
-plus = False
-while plus:
-    num1 = try_int()
-    num2 = try_int()
-    print(num1 + num2)
-    quit_ = input('do you want to quit? (y/n): ')
-    if quit_ == 'y':
-        break
-
 def print_line(file_name):
     try:
         with open(file_name, 'r', encoding='utf-8') as FILE:
+            #尝试读取文件，文件不存在时静默
             for LINE in FILE:
                 print(LINE)
     except FileNotFoundError:
         pass
+    #静默函数
 
-print_line('cats.txt')
-print_line('dogs.txt')
+if __name__ == '__main__':
+
+    plus = False
+    while plus:
+        num1 = try_int()
+        num2 = try_int()
+        print(num1 + num2)
+        quit_ = input('do you want to quit? (y/n): ')
+        if quit_ == 'y':
+            break
+
+    print_line('cats.txt')
+    print_line('dogs.txt')
+
+'''10-10略'''
