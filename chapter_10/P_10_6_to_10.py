8b9066031f1608fced06d2bfae037f22ab8cc815
raintle <2162951845@qq.com>
2026-01-19 15:06:31 +0800

    Changes

--- chapter_10/P_10_6_to_10.py
+++ chapter_10/P_10_6_to_10.py
@@ -11,7 +11,18 @@ def try_int():
             break
     return num
 
+def plus():
+
+    while True:
+        num1 = try_int()
+        num2 = try_int()
+        print(num1 + num2)
+        quit_ = input('do you want to quit? (y/n): ')
+        if quit_ == 'y':
+            break
+
 def print_line(file_name):
+
     try:
         with open(file_name, 'r', encoding='utf-8') as FILE:
             #尝试读取文件，文件不存在时静默
@@ -23,14 +34,7 @@ def print_line(file_name):
 
 if __name__ == '__main__':
 
-    plus = False
-    while plus:
-        num1 = try_int()
-        num2 = try_int()
-        print(num1 + num2)
-        quit_ = input('do you want to quit? (y/n): ')
-        if quit_ == 'y':
-            break
+    plus()
 
     print_line('cats.txt')
     print_line('dogs.txt')
