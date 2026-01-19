86c09ca8ae9db1202ea3de6c603148d30bb71d0f
raintle <2162951845@qq.com>
2026-01-19 19:54:50 +0800

    该练习完成

--- chapter_11/test_employee.py
+++ chapter_11/test_employee.py
@@ -1,15 +1,20 @@
 import unittest
-from 练习.chapter_11.employee import Employee
+from employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
 
+    def setUp(self):
+
+        self.employee = Employee('aaa', 'bbb', 7000)
+        #默认雇员实例
+
     def test_give_default_raise(self):
-        employee = Employee('aaa', 'bbb', 7000)
-        employee.give_raise()
-        self.assertEqual(employee.year_raise, 12000)
+        #默认年薪添加
+        self.employee.give_raise()
+        self.assertEqual(self.employee.year_raise, 12000)
 
     def test_give_custom_raise(self):
-        employee = Employee('aaa', 'bbb', 7000)
-        employee.give_raise(2500)
-        self.assertEqual(employee.year_raise, 9500)
+        #指定年薪添加
+        self.employee.give_raise(2500)
+        self.assertEqual(self.employee.year_raise, 9500)
